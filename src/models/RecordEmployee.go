c19ca0099c0a8d5f3cebd4f1c37aea021c66408a
matias-dias-brisa <matias.dias@grupobrisanet.com.br>
2024-02-13 08:29:45 -0300

    feat: Adicionado uma estrutura que junta os dosi modelos de funcionario e o controle de ponto

---
+++ src/models/RecordEmployee.go
@@ -0,0 +1,9 @@
+package models
+
+type RecordWithEmployee struct {
+	ID           uint64 `json:"id,omitempty"`
+	EmployeeCode uint64 `json:"codigo_funcionario" binding:"required"`
+	RecordType   string `json:"tipo_registro" binding:"required"`
+	Name         string `json:"nome,omitempty" binding:"required"`
+	Email        string `json:"email,omitempty" binding:"required"`
+}
