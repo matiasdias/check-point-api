e749446d47e47e2d7e591278f118b497c86c2546
matias-dias-brisa <matias.dias@grupobrisanet.com.br>
2024-03-06 16:11:40 -0300

    Removido o campo de tipo de registro

--- src/models/RecordEmployee.go
+++ src/models/RecordEmployee.go
@@ -1,10 +1,9 @@
 package models
 
-// RecordWithEmployee junção das duas structs(employee, recordPoint)
+// RecordWithEmployee responsável pelo retorno dos campos do registro de ponto e do funcionário
 type RecordWithEmployee struct {
 	ID           uint64 `json:"id,omitempty"`
 	EmployeeCode uint64 `json:"codigo_funcionario" binding:"required"`
-	RecordType   string `json:"tipo_registro" binding:"required"`
 	Name         string `json:"nome,omitempty" binding:"required"`
 	Email        string `json:"email,omitempty" binding:"required"`
 	Office       string `json:"office,omitempty" binding:"required"`
