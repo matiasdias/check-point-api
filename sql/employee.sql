5c5f7e5884d173b047f90a5e2b7b4e9ca553451b
matias-dias-brisa <matias.dias@grupobrisanet.com.br>
2024-03-13 13:01:51 -0300

    Adicionado a seed do administrador e a coluna referente ao administrador ou não

--- sql/employee.sql
+++ sql/employee.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS funcionario (
     id serial PRIMARY KEY, 
-    nome varchar(50) NOT NULL, 
-    email varchar(50) NOT NULL UNIQUE, 
+    is_admin boolean DEFAULT false,
+    nome varchar(250) NOT NULL, 
+    email varchar(100) NOT NULL UNIQUE, 
     telefone varchar(11) NOT NULL,
     cargo varchar(50) NOT NULL,
 	idade integer NOT NULL,   
@@ -10,3 +11,7 @@ CREATE TABLE IF NOT EXISTS funcionario (
     criadoem timestamp DEFAULT current_timestamp NOT NULL,
     updateem timestamp DEFAULT current_timestamp NOT NULL
 );
+
+INSERT INTO funcionario (nome, email, telefone, senha, idade, cpf, cargo, is_admin) 
+VALUES
+('Matias Dias', 'matias@gmail.com', '88997774641', '$2y$10$/tnAheWxR7TFmFg7M7UOUudkTGTuhwHFVigILJ.S2k5YizbXYbR3e', 26, '07174958338', 'Desenvolvedor back-end', true);
