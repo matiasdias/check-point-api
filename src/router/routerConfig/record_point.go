cc18827f7f9b1cca254bc5c32368fb20060e3eb4
matias-dias-brisa <matias.dias@grupobrisanet.com.br>
2024-03-13 13:05:11 -0300

    Alterado o nome do paramentro das rotas de registro de ponto

--- src/router/routerConfig/record_point.go
+++ src/router/routerConfig/record_point.go
@@ -21,21 +21,21 @@ var routesRecordPoint = []Routers{
 	},
 
 	{
-		URI:            "/recordPoint/{recordID}",
+		URI:            "/recordPoint/{recordEmployeeID}",
 		Method:         http.MethodGet,
 		Function:       controllers.ListIDRecordPoint,
 		Authencication: true,
 	},
 
 	{
-		URI:            "/recordPoint/{recordID}",
+		URI:            "/recordPoint/{recordEmployeeID}",
 		Method:         http.MethodPut,
 		Function:       controllers.UpdateRecordPoint,
 		Authencication: true,
 	},
 
 	{
-		URI:            "/recordPoint/{recordID}",
+		URI:            "/recordPoint/{recordEmployeeID}",
 		Method:         http.MethodDelete,
 		Function:       controllers.DeleteRecordPoint,
 		Authencication: true,
